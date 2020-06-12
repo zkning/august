e02640fadb2db5a2dbdc5f6882ee644cb5ef97b9
zkning <120716625@qq.com>
2020-06-13 07:54:21 +0800

    feat: dev

--- august-sharding-jdbc/src/main/java/com/august/sharding/mapper/UsersMapper.java
+++
@@ -1,9 +0,0 @@
-package com.august.sharding.mapper;
-
-import com.august.sharding.domain.Users;
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UsersMapper extends BaseMapper<Users> {
-}
