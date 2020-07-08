3eb1a340e4156a55e1be79890803e0bb002c1115
zkning <120716625@qq.com>
2020-07-08 23:31:06 +0800

    feat:  上传

---
+++ sample-message/src/test/java/com/ning/AppTest.java
@@ -0,0 +1,26 @@
+package com.ning;
+
+import static org.junit.Assert.assertTrue;
+
+import com.ning.message.MessageApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple MessageApplication.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MessageApplication.class})
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
